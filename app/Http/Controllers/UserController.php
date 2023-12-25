126c371c2d0551ebcd32398b1b38a2c3a9e1eb8a
Florenmae <magulodren@gmail.com>
2023-12-25 09:29:50 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+    public function createUser(Request $request){
+        $newUser = new user();
+
+        $newUser->id = $request->id;
+        $newUser->role = $request->role;
+        $newUser->name = $request->name;
+        $newUser->email = $request->email;
+        $newUser->password = $request->password;
+
+        $res = $newUser->save();
+
+        return $res;
+    }
+
+    public function getUsers(){
+
+        return user::select()->where('id', Auth::id())->get();
+    }
+
+
+}
